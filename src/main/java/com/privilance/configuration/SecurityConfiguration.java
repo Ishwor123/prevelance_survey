839e10a1f460028405bd36434c14fbd0eaef1c10
Ishwor Thapa <itss.ishwor@gmail.com>
2017-03-21 15:13:25 +0545

    full login and registration and dashboard

--- src/main/java/com/privilance/configuration/SecurityConfiguration.java
+++ src/main/java/com/privilance/configuration/SecurityConfiguration.java
@@ -73,7 +73,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	public void configure(WebSecurity web) throws Exception {
 	    web
 	       .ignoring()
-	       .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+	       .antMatchers("/resources/**", "/static/**","/bootstrap/**","/build/**","/dist/**","/plugins/**","/css/**", "/js/**", "/images/**");
 	}
 
 }
